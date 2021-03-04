46c403a66a3d9cd577d989d5c347692302d70418
Wrdlprmpf <58952423+Wrdlprmpf@users.noreply.github.com>
2021-03-04 19:03:35 +0100

    Create all necessary items
    Created buttons, textfields, startup animation and input fields

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -5,20 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText mEdit;
+    ImageView aau;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.button = findViewById(R.id.button_id1);
+        this.aau = findViewById(R.id.image1);
+        startUpAnimation();
 
-        final Button button = findViewById(R.id.button_id1);
         button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Log.i("MyApp","OnCLick Works");
+                Log.i("MyApp", mEdit.getText().toString());
+                startUpAnimation();
             }
         });
+
+        this.mEdit = (EditText)findViewById(R.id.InputText_1);
+
+        mEdit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                Log.v("EditText", mEdit.getText().toString());
+            }
+        });
+    }
+
+    private void startUpAnimation(){
+        Animation fadeInAnimation = AnimationUtils.loadAnimation(this,R.anim.fadein);
+        aau.startAnimation(fadeInAnimation);
     }
 }
