c6a4b0f538c4563df53c4d16cb6c358dac5dc516
Wrdlprmpf <58952423+Wrdlprmpf@users.noreply.github.com>
2021-03-04 17:32:16 +0100

    First tryout and experimenting
    First experiment with text input, images, buttons and event listeners

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.einzelbeispiel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button_id1);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Log.i("MyApp","OnCLick Works");
+            }
+        });
     }
 }
